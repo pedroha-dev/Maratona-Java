a833133852ffbce2de89146bb2e7b4a1326868c7
pedro henrique <pedroha_@outlook.com.br>
2026-02-02 21:55:58 -0300

    Aprendendo a usar números inteiros e decimais

--- src/EstudoTipos.java
+++ src/EstudoTipos.java
@@ -1,2 +1,16 @@
 public class EstudoTipos {
+    public static void main(String[] args){
+
+        //Teste de Precisão
+        int numA = 10;
+        int numB = 3;
+
+        System.out.println("Resultado da divisão inteira: " + (numA / numB));
+
+        double numC = 10.0;
+        double  numD = 3.0;
+
+        System.out.println("Resultado da divisão com double: " + (numC / numD));
+
+    }
 }
